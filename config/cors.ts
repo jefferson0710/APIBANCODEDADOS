b3db12dfde784c37e52b765f63c0cc7185627c95
jefferson <jefferson_nascimento07@outlook.com>
2024-12-09 22:04:09 -0300

    colocandolink

--- config/cors.ts
+++ config/cors.ts
@@ -44,7 +44,7 @@ const corsConfig: CorsConfig = {
   |                     one of the above values.
   |
   */
-  origin: ['http://localhost:4200'],
+  origin: ['https://fichadeanamenese.netlify.app/'],
 
   /*
   |--------------------------------------------------------------------------
